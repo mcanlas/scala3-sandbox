0b1b0865d84829168dd65b87b0c4534f4eb77f5c
Mark Canlas <github@htmlism.com>
2023-10-25 13:50:32 -0400

    min

--- contra/src/main/scala/ContraVariantSandboxApp.scala
+++ contra/src/main/scala/ContraVariantSandboxApp.scala
@@ -1,20 +1,16 @@
 trait Contravariant[F[_]]:
   def contramap[A, B](fa: F[A])(f: B => A): F[B]
 
-trait Encoder[Z, A]:
-  def encode(x: A): Z
+trait Encoder[Z, A]
 
 object Encoder:
-  given Encoder[String, String] with
-    def encode(x: String): String =
-      x + x
+  given Encoder[String, String] =
+    new Encoder[String, String] {}
 
   // when written Encoder[*, Z], this lookup starts to fail. why?
   given [Z]: Contravariant[Encoder[Z, *]] with
     def contramap[A, B](fa: Encoder[Z, A])(f: B => A): Encoder[Z, B] =
-      new Encoder[Z, B]:
-        def encode(x: B): Z =
-          fa.encode(f(x))
+      new Encoder[Z, B] { }
 
   extension[F[_] : Contravariant, A] (fa: F[A])
     def demandContravariance: Unit =
