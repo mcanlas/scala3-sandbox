ed66ffd12f890ac4658d6b1f054a1041506e7380
Mark Canlas <github@htmlism.com>
2023-10-25 13:28:14 -0400

    this compiles

--- build.sbt
+++ build.sbt
@@ -1,1 +1,9 @@
 scalaVersion := "3.3.1"
+
+lazy val contra =
+  project
+    .settings(
+      scalaVersion := "3.3.1",
+      libraryDependencies += "org.typelevel" %% "cats-core" % "2.10.0",
+      scalacOptions += "-Ykind-projector"
+    )
---
+++ contra/src/main/scala/ContraVariantSandboxApp.scala
@@ -0,0 +1,20 @@
+import cats._
+import cats.syntax.all._
+
+trait Encoder[Z, A]:
+  def encode(x: A): Z
+
+object Encoder:
+  given Encoder[String, String] with
+    def encode(x: String): String =
+      x + x
+
+  given [Z]: Contravariant[Encoder[Z, *]] with
+    def contramap[A, B](fa: Encoder[Z, A])(f: B => A): Encoder[Z, B] =
+      new Encoder[Z, B]:
+        def encode(x: B): Z =
+          fa.encode(f(x))
+
+object ContraVariantSandboxApp extends App:
+  summon[Encoder[String, String]]
+    .contramap((n: Int) => n.toString)
